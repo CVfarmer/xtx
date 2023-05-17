6fd90bb8f1ee132bbcb9bdb106d33bb2e442d579
wanghaoxian <1983309418@qq.com>
2023-05-17 22:54:11 +0800

    home-懒加载指令优化

---
+++ src/directives/index.js
@@ -0,0 +1,28 @@
+//封装懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'  //vueUse插件 用于懒加载
+
+export const lazyPlugin = {
+    install(app){
+        //懒加载指令逻辑
+        //  此指令用于懒加载  插件内容：useIntersectionObserver 
+app.directive('img-lazy',{
+    mounted(el,binding){
+        //el:指令绑定的那个元素 --->img
+        //binding：指令对象， 常用的是binding.value  指令等于号后面绑定的表达式的值---> 图片url
+        console.log(el,binding);
+     
+        const {stop}= useIntersectionObserver(
+            el,//监听对象是el  el就是img
+            ([{ isIntersecting }]) => {    //isIntersecting代表当前监听的图片是否进入视图区域
+                console.log(isIntersecting);  //测试图片出现在视窗为true或者false
+                if(isIntersecting){
+                    el.src = binding.value     //若为真，给图片的src赋值一个有效的图片地址（发送网络请求），地址就在binding.value存着
+                    stop()                //当第一次加载完就暂停监听，否则造成内存浪费
+                }
+            },
+          
+          )
+    }
+})
+    }
+}
--- src/main.js
+++ src/main.js
@@ -2,37 +2,20 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { useIntersectionObserver } from '@vueuse/core'  //vueUse插件 用于懒加载
 
 import App from './App.vue'
 import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
-
+//引入自定义懒加载插件
+import {lazyPlugin } from '@/directives/index'
 
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)  //注册自定义懒加载插件
 
 app.mount('#app')
 
-//定义全局全局指令  此指令用于懒加载  插件内容：useIntersectionObserver 
-app.directive('img-lazy',{
-    mounted(el,binding){
-        //el:指令绑定的那个元素 --->img
-        //binding：指令对象， 常用的是binding.value  指令等于号后面绑定的表达式的值---> 图片url
-        console.log(el,binding);
-        useIntersectionObserver(
-            el,//监听对象是el  el就是img
-            ([{ isIntersecting }]) => {    //isIntersecting代表当前监听的图片是否进入视图区域
-                console.log(isIntersecting);  //测试图片出现在视窗为true或者false
-                if(isIntersecting){
-                    el.src = binding.value     //若为真，给图片的src赋值一个有效的图片地址（发送网络请求），地址就在binding.value存着
-                }
-            },
-          
-          )
-    }
-})
