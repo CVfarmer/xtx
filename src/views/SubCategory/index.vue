638fe1461a6e5012809518028d67c2ed59e6b5fc
wanghaoxian <1983309418@qq.com>
2023-05-20 14:50:16 +0800

    二级分类-列表无限加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -10,12 +10,12 @@
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
          <!-- 商品列表-->
          <GoodsItem v-for="goods in goodList" :goods="goods"  :key="goods.id"/>
       </div>
@@ -55,6 +55,30 @@ const getGoodList = async () =>{
  goodList.value = res.result.items
 }
 onMounted(()=>getGoodList() )
+
+
+//tab切换回调
+const tabChange = ()=>{
+  console.log('tab切换',reqData.value.sortField);
+  reqData.value.page = 1
+  getGoodList()
+}
+
+
+//列表无限加载
+const disabled = ref(false)
+const load = async()=>{
+  console.log(123);
+  //获取下一页的数据
+  reqData.value.page++    //页数加一
+  const res = await getSubCategoryAPI(reqData.value)        //获取新一页的数据
+ goodList.value =  [...goodList.value,...res.result.items]  //新老数据拼接
+
+ //加载完毕，停止监听
+ if(res.result.items.length === 0){
+  disabled.value = true
+ }
+}
 </script>
 
 <style lang="scss" scoped>
