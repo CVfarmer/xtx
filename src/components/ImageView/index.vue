b1a18ca611e33077924f9b1430f15ddd917d0deb
wanghaoxian <1983309418@qq.com>
2023-05-21 15:17:50 +0800

    图片预览组件-props适配以及优化操作

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -29,14 +29,22 @@ import {ref, watch} from "vue"
 import { useMouseInElement } from '@vueuse/core'  //useMouseElement插件
 
 
+// props适配图片列表
+defineProps({
+  imageList:{
+    type:Array,
+    default:() => []
+  }
+})
+
 // 图片列表
-const imageList = [
+/* const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
   "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
   "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
   "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
   "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
+] */
 
 //1.小图切换大图
 const activeIndex = ref(0)
@@ -58,7 +66,7 @@ const positionX = ref(0)
 const positionY = ref(0)
 
 watch([elementX,elementY,isOutside],()=>{
-if(isOutside.value) return
+if(isOutside.value) return  //若鼠标不在图片里面，不在执行下面代码，（优化操作）
 
   //有效距离内控制滑块距离
   //横向
