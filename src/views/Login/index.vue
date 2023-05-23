d140fd7b77f133b41624cddcdb081f5c3a34bdd2
wanghaoxian <1983309418@qq.com>
2023-05-23 14:41:04 +0800

    登录-表单验证-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -21,7 +21,7 @@
           <div class="form">
             <!-- 1.:model="form" :rules="rules" 绑定规则对象 -->
             <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
-              <!-- 2.通过prop指定表单域的校验字段名 -->
+              <!-- 2.通过prop指定校验规则 -->
               <el-form-item prop="account" label="账户">
                 <!-- 3.把表单对象进行双向绑定 -->
                 <el-input v-model="form.account"/>
@@ -29,8 +29,8 @@
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item label-width="22px" prop="agree">
+                <el-checkbox  size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
@@ -59,13 +59,17 @@
 </template>
 
 <script setup>
+//简单校验只需要通过配置即可，特殊校验需要自定义校验规则
+//这是基于element的校验规则
+
 //表单验证（账号名+密码）
 import {ref} from 'vue'
 
 //1.准备表单对象
 const form = ref({
   account: '',
-  password: ''
+  password: '',
+  agree:true 
 })
 
 //2.准备规则对象
@@ -77,7 +81,21 @@ const rules = {
     { required: true, message: '密码不能为空', trigger: 'blur' },
     { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' },
   ],
-
+ agree:[
+  {
+    validator:(rule,value,callback) => {
+      console.log(value);
+      //自定义校验逻辑
+      //勾选通过，反之不通过
+ 
+    if (value) {
+          callback()
+        } else {
+          callback(new Error('请勾选协议'))
+        }
+    }
+  }
+ ]
 }
 
 </script>
